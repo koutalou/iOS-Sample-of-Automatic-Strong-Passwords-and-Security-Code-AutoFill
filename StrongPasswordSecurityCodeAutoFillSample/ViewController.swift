aaa71c662c7c864660c494a3c5a00ebf8198cee0
koutalou <k.koutalou@gmail.com>
2018-12-06 14:46:25 +0900

    :hatching_chick: Initial commit

---
+++ StrongPasswordSecurityCodeAutoFillSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StrongPasswordSecurityCodeAutoFillSample
+//
+//  Created by koutalou on 2018/12/06.
+//  Copyright © 2018年 koutalou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
